5403f4db19085b60a7722687d6c3ef0b979ec0f4
Ezra Stein <ezstein10@gmail.com>
2016-05-27 11:17:57 -0400

    Added Preferences

--- src/xyz/ezstein/backend/util/Util.java
+++ src/xyz/ezstein/backend/util/Util.java
@@ -1,6 +1,11 @@
 package xyz.ezstein.backend.util;
 
-import java.util.concurrent.TimeUnit;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import xyz.ezstein.backend.app.Locator;
 
 /**
  * A utility class
@@ -9,6 +14,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class Util {
 	
+	public static final String CONFIG_FILE = "config.properties";
 	/**
 	 * Converts nanoseconds into a human readable time representation (hh:mm:ss)
 	 * @param nanos
@@ -26,5 +32,69 @@ public class Util {
 		long milliseconds = TimeUnit.NANOSECONDS.toMillis(nanos)-TimeUnit.SECONDS.toMillis(TimeUnit.NANOSECONDS.toSeconds(nanos));
 		out+=hours+":"+minutes+":"+seconds+":"+milliseconds;
 		return out;
+	}
+	
+	public static String getProperty(String key){
+		Properties properties = new Properties();
+		try {
+			if(!Locator.exists(CONFIG_FILE)){
+				properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+			} else {
+				properties.load(Files.newInputStream(Locator.locateFile(CONFIG_FILE)));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return properties.getProperty(key);
+	}
+	
+	public static Properties getProperties(){
+		Properties properties = new Properties();
+		try {
+			if(!Locator.exists(CONFIG_FILE)){
+				properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+			} else {
+				properties.load(Files.newInputStream(Locator.locateFile(CONFIG_FILE)));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return properties;
+	}
+	
+	public static void setProperty(String key, String value){
+		Properties properties = new Properties();
+		try {
+			if(!Locator.exists(CONFIG_FILE)){
+				properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+			} else {
+				properties.load(Files.newInputStream(Locator.locateFile(CONFIG_FILE)));
+			}
+			properties.setProperty(key,value);
+			properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void setProperties(Map<String, String> props){
+		Properties properties = new Properties();
+		try {
+			if(!Locator.exists(CONFIG_FILE)){
+				properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+			} else {
+				properties.load(Files.newInputStream(Locator.locateFile(CONFIG_FILE)));
+			}
+			for(String key : props.keySet()){
+				properties.setProperty(key, props.get(key));
+			}
+			properties.store(Files.newOutputStream(Locator.locateFile(CONFIG_FILE)),"");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/xyz/ezstein/fx/preferences/PreferencesController.java
+++ src/xyz/ezstein/fx/preferences/PreferencesController.java
@@ -1,22 +1,59 @@
 package xyz.ezstein.fx.preferences;
 
+
+import java.util.*;
+
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.scene.control.*;
 import javafx.stage.*;
+import xyz.ezstein.backend.util.Util;
 
 public class PreferencesController {
 	
 	private Stage stage;
+	@FXML private TextField startTextField;
+	@FXML private TextField pauseTextField;
+	@FXML private TextField stopTextField;
+	@FXML private TextField splitTextField;
+	@FXML private TextField backgroundTextField;
+	@FXML private TextField delayTextField;
+	
 	public void initializeAsGUI(Stage stage){
 		this.stage=stage;
+		Properties props = Util.getProperties();
+		startTextField.setText(props.getProperty("start"));
+		stopTextField.setText(props.getProperty("stop"));
+		splitTextField.setText(props.getProperty("split"));
+		pauseTextField.setText(props.getProperty("pause"));
+		
+		startTextField.setOnKeyReleased(ke->{
+			startTextField.setText(ke.getCode().toString());
+		});
+		pauseTextField.setOnKeyReleased(ke->{
+			pauseTextField.setText(ke.getCode().toString());
+		});
+		splitTextField.setOnKeyReleased(ke->{
+			splitTextField.setText(ke.getCode().toString());
+		});
+		stopTextField.setOnKeyReleased(ke->{
+			stopTextField.setText(ke.getCode().toString());
+		});
+		
 	}
 	
 	@FXML
 	public void okButtonClick(ActionEvent ae){
-		
+		HashMap<String,String> props = new HashMap<String,String>();
+		props.put("start", startTextField.getText());
+		props.put("stop", stopTextField.getText());
+		props.put("split", splitTextField.getText());
+		props.put("pause", pauseTextField.getText());
+		Util.setProperties(props);
+		stage.close();
 	}
 	@FXML
 	public void cancelButtonClick(ActionEvent ae){
-		
+		stage.close();
 	}
 }
